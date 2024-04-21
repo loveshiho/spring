89681491e5900088d25db26b4aff3acd8d5bdd00
loveshiho <1822881521@qq.com>
2024-04-21 14:51:48 +0800

    测试spring事务控制——>注解方式实现

--- springTest07/src/main/java/com/akai/service/impl/AccountServiceImpl.java
+++ springTest07/src/main/java/com/akai/service/impl/AccountServiceImpl.java
@@ -4,16 +4,19 @@ import com.akai.dao.AccountDao;
 import com.akai.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+// @Transactional // 加在类上,代表类中的所有方法都添加了事务控制
 public class AccountServiceImpl implements AccountService {
     @Autowired
     private AccountDao accountDao;
-
+    @Transactional  // 放在方法上,就是仅仅对当前方法添加了事务控制
     @Override
     public int transMoney(int from, int to, int money) {
         int rows = 0;
         rows += accountDao.transMoney(from, 0 - money);
+        int i  = 1 / 0;
         rows += accountDao.transMoney(to, money);
         return rows;
     }
